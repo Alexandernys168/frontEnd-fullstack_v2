4859b6778952b4b6d0b965e859d1d86bd8784aee
miladkth <116656210+miladkth@users.noreply.github.com>
2023-11-17 13:51:23 +0100

    added doctor, others, added relation in encounter

--- back/src/main/java/kth/milad/entity/Encounter.java
+++ back/src/main/java/kth/milad/entity/Encounter.java
@@ -3,6 +3,8 @@ package kth.milad.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,4 +17,19 @@ public class Encounter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @OneToMany(mappedBy = "encounter", cascade = CascadeType.ALL)
+    private List<Observation> observations;
+
+    @ManyToOne
+    @JoinColumn(name= "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name= "doctor_id")
+    private Doctor doctor;
+
+    @ManyToOne
+    @JoinColumn(name= "other_id")
+    private Others others;
 }
---
+++ back/src/main/java/kth/milad/repository/OthersRepository.java
@@ -0,0 +1,8 @@
+package kth.milad.repository;
+
+import kth.milad.entity.Others;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OthersRepository extends JpaRepository<Others,Integer> {}
