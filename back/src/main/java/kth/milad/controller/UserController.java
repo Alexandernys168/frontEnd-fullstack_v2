cd70ae35ee3d52fc231ce0ff0611bea834c2ada3
miladkth <116656210+miladkth@users.noreply.github.com>
2023-11-20 10:07:09 +0100

    v1

--- back/src/main/java/kth/milad/controller/UserController.java
+++ back/src/main/java/kth/milad/controller/UserController.java
@@ -5,6 +5,7 @@ import kth.milad.entity.Doctor;
 import kth.milad.entity.Others;
 import kth.milad.entity.Patient;
 import kth.milad.service.IService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,15 +16,19 @@ import java.util.Objects;
 @CrossOrigin
 public class UserController {
 
+    @Autowired
     private IService<Doctor> doctorService;
+    @Autowired
     private IService<Others> othersService;
+    @Autowired
     private IService<Patient> patientService;
 
 
     @PostMapping("/register")
     public void RegisterUser(@RequestBody UserVm userVm){
         switch (userVm.getUserType()){
-            case "DOCTOR": doctorService.create(new Doctor(userVm.getId(), userVm.getName(), userVm.getEmail(), userVm.getPassword()));break;
+            case "DOCTOR": doctorService.create(new Doctor(userVm.getId(), userVm.getName(), userVm.getEmail(), userVm.getPassword()));
+                System.out.println(userVm.getEmail());break;
             case "OTHERS": othersService.create(new Others(userVm.getId(), userVm.getName(), userVm.getEmail(), userVm.getPassword()));break;
             case "PATIENT": patientService.create(new Patient(userVm.getId(), userVm.getName(), userVm.getEmail(), userVm.getPassword()));break;
         }
@@ -32,9 +37,25 @@ public class UserController {
 
     @PostMapping("/login")
     public UserVm loggInUser(@RequestBody UserVm userVm){
-        List<UserVm> userVms = getAllUsers();
+        List<UserVm> userVms = new ArrayList<>();
+        List<Doctor> doctors = doctorService.getAll();
+        for (Doctor d:doctors) {
+            userVms.add(new UserVm(d.getId(),d.getName(),"DOCTOR",d.getEmail(),d.getPassword()));
+        }
+
+        List<Others> others = othersService.getAll();
+        for (Others o:others) {
+            userVms.add(new UserVm(o.getId(),o.getName(),"OTHERS",o.getEmail(),o.getPassword()));
+        }
+
+
         for (UserVm u:userVms) {
-            if (Objects.equals(u.getEmail(), userVm.getEmail()) && Objects.equals(u.getPassword(), userVm.getPassword())){
+            if (u.getEmail() == null || userVm.getEmail() == null) {
+                UserVm errorNoUser = new UserVm();
+                errorNoUser.setName("NO USER FOUND");
+                continue;
+            }
+            if (u.getEmail().equals(userVm.getEmail()) && u.getPassword().equals(userVm.getPassword())){
                 return u;
             }
         }
